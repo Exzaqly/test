429eb659655f840ecc170d28f38d4b596558a84c
Exzaqly <Gryab4ik@mail.ru>
2022-12-06 13:37:53 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,92 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Param {
+  id: number;
+  name: string;
+  type: 'string';
 }
 
-export default App;
+interface ParamValue {
+  paramId: number;
+  value: string;
+}
+
+interface Model {
+  paramValues: ParamValue[];
+  // в задании не описан интерфейс Color
+  //colors: Color[]
+}
+
+interface Props {
+  params: Param[];
+  model: Model;
+}
+
+type State = Record<string, ParamValue['value']>
+
+type ParamsMap = Record<string, Pick<Param, 'name' | 'type'>>
+
+export class ParamEditor extends React.Component<Props, State> {
+  paramsMap: ParamsMap = {}
+
+  constructor(props: Props) {
+    super(props)
+
+    this.paramsMap = props.params.reduce((res, next) => {
+      res[next.id] = { name: next.name, type: next.type }
+      return res
+    }, {} as ParamsMap)
+
+    this.state = props.model.paramValues.reduce((res, next) => {
+      res[next.paramId] = next.value
+      return res
+    }, {} as State)
+  }
+
+  public getModel(): Model {
+    return {
+      paramValues: Object.entries(this.state).map(([id, value]) => ({ paramId: +id, value })),
+    }
+  }
+
+  onValueChange = (id: number, value: ParamValue['value']) => {
+    this.setState({ [id]: value })
+  }
+
+  render() {
+    return (
+      <form>
+        {this.props.model.paramValues.map(v => <div key={v.paramId} style={{
+          display: 'flex',
+          justifyContent: 'center',
+          marginBottom: '5px',
+        }}>
+          <div style={{ width: '150px' }}>{this.paramsMap[v.paramId].name}</div>
+          <ParamValueInput paramId={v.paramId}
+                           value={this.state[v.paramId]}
+                           type={this.paramsMap[v.paramId].type}
+                           onChange={this.onValueChange} />
+        </div>)}
+      </form>
+    )
+  }
+}
+
+type InputProps = ParamValue & {
+  onChange: (id: number, value: ParamValue['value']) => void
+  type: Param['type']
+}
+
+const ParamValueInput: FC<InputProps> = ({ onChange, paramId, value, type }) => {
+  switch (type) {
+    case 'string':
+      return (
+        <input onChange={(e) => {
+          onChange(paramId, e.target.value)
+        }} type='text' value={value} />
+      )
+  }
+}
+
+
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { ParamEditor } from "../../untitled10/src/App";
+import reportWebVitals from "./reportWebVitals";
+
+const params = [
+  {
+    id: 1,
+    name: "Назначение",
+    type: 'string' as const
+  },
+  {
+    id: 2,
+    name: "Длина",
+    type: 'string' as const
+  } 
+]
+
+const model = {
+  paramValues: [
+    {
+      paramId: 1,
+      value: "повседневное"
+    },
+    {
+      paramId: 2,
+      value: "макси"
+    }
+  ]
+}
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ParamEditor params={params} model={model} />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
